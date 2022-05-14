1df1e1455061d5d8b0d6702b45b4a2cc0133ff6d
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-05-14 20:23:02 +0300

    Createand define app instance

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+
+
+class Config:
+
+    pass
+
+class ProdConfig(Config):
+
+    pass
+
+class DevConfig(Config):
+    DEBUG = True
+
+
+
+config_options = {
+    "production": ProdConfig,
+    "development": DevConfig
+} 
---
+++ manage.py
@@ -0,0 +1,9 @@
+from app import create_app
+import app
+
+app = create_app()
+
+
+if __name__ == '__main__':
+
+    app.run()
