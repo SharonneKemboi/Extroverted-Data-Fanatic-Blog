da0c25238049a1602826f082d45871968970e3b8
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-05-17 13:27:26 +0300

    Rearrange the file content

--- app/__init__.py
+++ app/__init__.py
@@ -1,35 +1,47 @@
-from ensurepip import bootstrap
 from flask import Flask
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_login import LoginManager
-
-
-bootstrap = Bootstrap()
-db =SQLAlchemy()
-
+from flask_mail import Mail
+# from flask_simplemde import SimpleMDE
 
 
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+
+
+#initialize extensions
+bootstrap = Bootstrap()
+db =SQLAlchemy()
+# simple = SimpleMDE()
+mail = Mail()
 photos = UploadSet('photos',IMAGES)
 
 
 
 def create_app(config_name):
 
-# Creating the app configurations
+
+ #instantiate Flask
     app = Flask(__name__)
+
+    # Creating the app configurations
     app.config.from_object(config_options[config_name])
 
 # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
+    # simple.init_app(app)
+
+
+      # configure UploadSet
+    configure_uploads(app,photos)
 
 
 # Registering the blueprint
@@ -40,8 +52,7 @@ def create_app(config_name):
     app.register_blueprint(auth_blueprint,url_prefix ='/authenticate')
 
 
-  # configure UploadSet
-    configure_uploads(app,photos)
+
 
 
     return app
