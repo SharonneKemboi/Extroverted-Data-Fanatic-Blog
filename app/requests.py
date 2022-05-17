ece65d7a40ffbd511779cd539ef95b97079ec82c
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-05-17 13:33:12 +0300

    Modify Requests and consume a random quotes API in our function

--- app/requests.py
+++ app/requests.py
@@ -1,17 +1,8 @@
-import urllib.request,json
 
-def configure_request(app):
-    pass
+import requests
 
-def get_quote():
-   '''
-   Function that gets the json response to our url request
-   '''
-   get_randomquotes_url = 'http://quotes.stormconsultancy.co.uk/random.json'
-
-   with urllib.request.urlopen(get_randomquotes_url) as url:
-       quotes = url.read()
-       get_sources_response = json.loads(quotes)
-       print(quotes)
-
-   return get_sources_response 
+def random_post():
+    url='http://quotes.stormconsultancy.co.uk/random.json'
+    pos= requests.get(url)
+    posts= pos.json()
+    return posts
